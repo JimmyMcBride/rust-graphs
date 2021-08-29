69288e013d9c2fade9351a7dfc9d7e75e37b1fc2
JimmyMcBride <mcbride967@gmail.com>
2021-08-29 10:40:52 -0500

    saving progress

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::hash::Hash;
 
 /// A graph structure that contains vertices and the edges between them...
 pub struct Graph<T> {
-  pub vertices: HashMap<T, HashSet<T>>,
+  pub vertices: HashMap<T, Vec<T>>,
 }
 
 impl<T> Graph<T>
@@ -19,22 +19,18 @@ where
   }
   /// Adds a new vertex to the graph...
   pub fn add_vertex(&mut self, vertex: T) {
-    self.vertices.insert(vertex, HashSet::new());
+    self.vertices.insert(vertex, Vec::new());
   }
   /// Add and edge between two vertices...
   pub fn add_edge(&mut self, vertex_one: T, vertex_two: T) {
     if self.vertices.contains_key(&vertex_one) && self.vertices.contains_key(&vertex_two) {
-      self
-        .vertices
-        .get_mut(&vertex_one)
-        .unwrap()
-        .insert(vertex_two);
+      self.vertices.get_mut(&vertex_one).unwrap().push(vertex_two);
     } else {
       panic!("Vertex doesn't exist... ☠️")
     }
   }
   /// Gets all vertices connected to a given vertex...
-  pub fn get_neighbors(&self, vertex: T) -> &HashSet<T> {
+  pub fn get_neighbors(&self, vertex: T) -> &Vec<T> {
     match self.vertices.get(&vertex) {
       Some(n) => n,
       None => panic!("Vertex '{:#?}' does not exist", vertex),
@@ -61,7 +57,7 @@ where
       }
       for (curr_vert, next_vert) in self.vertices.get_key_value(curr_item.last().unwrap()) {
         println!("Curr vert: {:#?}", curr_vert);
-        println!("Next vert: {}", next_vert);
+        println!("Next vert: {}", next_vert[0]);
         // for vert in next_vert.iter() {
         //   curr_item.push(self.vertices.get_key_value(vert).unwrap());
         // }
